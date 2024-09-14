b050f8c87e796202132fe97d6243997eccd2af08
AlexeyARyabov <98357572+AlexeyARyabov@users.noreply.github.com>
2024-09-14 23:17:50 +0300

    Add files via upload

--- module_9_1.py
+++ module_9_1.py
@@ -5,19 +5,6 @@ def apply_all_func(int_list, *functions):
         results.update({func.__name__: func_value})
     return results
 
-def min_el(int_list):
-    return min(int_list)
 
-def max_el(int_list):
-    return min(int_list)
-
-def len_list(int_list):
-    return len(int_list)
-
-def sum_el(int_list):
-    return sum(int_list)
-
-def sorted_el(int_list):
-    return sorted(int_list)
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
