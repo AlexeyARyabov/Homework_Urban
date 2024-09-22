3aad4f71c09ca21b92e8523f0a707d53b27c21c0
AlexeyARyabov <98357572+AlexeyARyabov@users.noreply.github.com>
2024-09-22 21:29:27 +0300

    Add files via upload

--- module_9_6.py
+++ module_9_6.py
@@ -1,13 +1,13 @@
-from itertools import product, repeat
+from itertools import  combinations
 
 
 def all_variants(text):
     i = 1
     while i <= len(text):
-        x = product(text, repeat=i)
-        yield x
+        for comb in combinations(text, i):
+            yield ''.join(comb)
         i += 1
 
-a = all_variants("abc")
-for i in a:
-    print(i)
+a = all_variants('abc')
+for x in a:
+    print(x)
