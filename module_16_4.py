cafde0441d5cf4908eee19e70a8a61fd342b50a2
AlexeyARyabov <98357572+AlexeyARyabov@users.noreply.github.com>
2024-11-12 09:13:21 +0300

    Add files via upload

--- module_16_4.py
+++ module_16_4.py
@@ -1,6 +1,7 @@
 from typing import Annotated, List
 from fastapi import FastAPI, Path, status, Body, HTTPException
 from pydantic import BaseModel
+import uvicorn
 
 
 app = FastAPI()
@@ -17,8 +18,8 @@ async def all_users() -> List[User]:
     return users
 
 @app.post('/user/{username}/{age}')
-async def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')]
-                       , age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='23')])-> User:
+async def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')]
+                       , age: Annotated[int, Path(ge=18, le=120, description='Enter age')])-> User:
     if not users:
         new_id = 1
     else:
@@ -29,8 +30,8 @@ async def create_user(username: Annotated[str, Path(min_length=5, max_length=20,
 
 @app.put('/user/{user_id}/{username}/{age}')
 async  def update_user(user_id: Annotated[int, Path(ge=1)]
-                       , username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')]
-                       , age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='23')]) -> User:
+                       , username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')]
+                       , age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
     try:
         if users[user_id - 1]:
             users[user_id - 1] = User(id=user_id, username=username, age=age)
@@ -42,7 +43,13 @@ async  def update_user(user_id: Annotated[int, Path(ge=1)]
 @app.delete('/user/{user_id}')
 async def deleted_user(user_id: Annotated[int, Path(ge=1)]) -> User:
     try:
+        print('step1')
         if users[user_id - 1].id == user_id:
+            print('step2')
             return users.pop(user_id - 1)
     except IndexError:
         raise HTTPException(status_code=404, detail="User was not found")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
